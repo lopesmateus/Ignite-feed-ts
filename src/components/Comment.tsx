47a8ebd722741a7dfff1a6acf641b0286b65960c
Mateus Lopes <mts.lopes01@gmail.com>
2022-09-28 13:56:26 -0300

    First commit

---
+++ src/components/Comment.tsx
@@ -0,0 +1,55 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
+import { Avatar } from './Avatar';
+import styles from './Comment.module.css'
+
+interface CommentProps {
+    content: string;
+    onDeleteComment: (comment: string) => void;
+}
+
+export function Comment ({content, onDeleteComment}: CommentProps) {
+
+    const [ likeCount, setLikeCount ] = useState(0);
+    
+    function handleDeleteComment() {
+        onDeleteComment(content);
+
+    }
+
+    function handleLikeComment () {
+        setLikeCount((state) => {
+            return state + 1
+        })
+    }
+
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://github.com/lopesmateus.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Mateus Lopes</strong>
+                            <time title='21 de Setembro as 15:55h' dateTime='2022-09-21 15:55:00'>Cerca de 1h atrás</time>
+                        </div>
+
+                        <button onClick={handleDeleteComment} title='Deletar comentário'>
+                            <Trash size={24}/>
+                        </button>
+                    </header>
+
+                    <p>{content}</p>
+                </div>
+
+                <footer>
+                    <button onClick={handleLikeComment}>
+                        <ThumbsUp />
+                        Aplaudir <span>{likeCount}</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    );
+}
